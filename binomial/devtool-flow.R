626a02cf1be261bda07a2205790b65c86c60c22b
yhed10123 <48308790+yhed10123@users.noreply.github.com>
2019-05-01 12:28:00 -0700

    Add files via upload

---
+++ binomial/devtool-flow.R
@@ -0,0 +1,14 @@
+# =====================================================
+# Devtools binomial Distribution workflows
+# library(devtools)
+# Description: The following commands are used to create my own Binomial package
+# =====================================================
+
+devtools::document()          # generate documentation
+devtools::check_man()         # check documentation
+devtools::test()              # run tests
+devtools::build_vignettes()   # build vignettes
+devtools::build()             # build bundle
+devtools::install()           # install package
+devtools::check()             # check
+
